c5f9fd70ebd35c859c44c9526fc80991072cb591
wilson kao <wilkao036@gmail.com>
2021-03-30 18:13:59 +0800

    完成網速輸入及轉換功能

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState(0);
+
   return (
     <div className="container">
       <div className="card-header">Network Speed Converter</div>
@@ -17,7 +19,15 @@ function App() {
         <div className="converter">
           <div className="flex-1">
             <div className="converter-title">Set</div>
-            <input type="number" className="input-number" min="0" />
+            <input
+              type="number"
+              className="input-number"
+              min="0"
+              value={inputValue}
+              onChange={(e) => {
+                setInputValue(Number(e.target.value));
+              }}
+            />
           </div>
           <span className="angle-icon fa-2x" style={{ marginTop: 30 }}>
             <i className="fas fa-angle-right"></i>
@@ -27,7 +37,8 @@ function App() {
             <input
               type="text"
               className="input-number text-right"
-              value="125"
+              value={inputValue / 8}
+              disabled
             />
           </div>
         </div>
