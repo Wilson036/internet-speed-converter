3085ba3dff1cf366e9171af032d32f55759f5292
wilson kao <wilkao036@gmail.com>
2021-03-30 18:27:50 +0800

    split componets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
+import UnitControl from 'components/unitControl';
+import CardFooter from 'components/cardFooter';
 
 function App() {
   const [inputValue, setInputValue] = useState(0);
@@ -8,14 +10,7 @@ function App() {
     <div className="container">
       <div className="card-header">Network Speed Converter</div>
       <div className="card-body">
-        <div className="unit-control">
-          <div className="unit">Mbps</div>
-          <span className="exchange-icon fa-fw fa-stack">
-            <i className="far fa-circle fa-stack-2x"></i>
-            <i className="fas fa-exchange fa-stack-1x"></i>
-          </span>
-          <div className="unit">MB/s</div>
-        </div>
+        <UnitControl />
         <div className="converter">
           <div className="flex-1">
             <div className="converter-title">Set</div>
@@ -43,7 +38,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="card-footer">FAST</div>
+      <CardFooter />
     </div>
   );
 }
---
+++ src/components/cardFooter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CardFooter() {
+  return <div className="card-footer">FAST</div>;
+}
+
+export default CardFooter;
